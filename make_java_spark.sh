a0a3e9469c6e2f8f05a1dd7fe79b923e647d7b10
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-05-28 15:47:13 +0900

    環境作成shellを作成

---
+++ make_java_spark.sh
@@ -0,0 +1,37 @@
+mkdir ./sample_java_spark
+cd sample_java_spark
+
+gradle init
+
+mkdir -p ./src/main/java/init
+echo -e \
+'package init;\n'\
+'\n'\
+'import static spark.Spark.*;\n'\
+'\n'\
+'public class Server {\n'\
+'\tpublic static void main(String[] args) {\n'\
+'\t\tport(3000);\n'\
+'\t\tSystem.out.println("http://localhost:3000/hello");\n'\
+'\t\tget("/hello", (request, response) -> "hello world");\n'\
+'\n'\
+'\t\tSystem.out.println("http://localhost:3000/json");\n'\
+'\t\tget("/json", "application/json", (request, response) -> "{\"message\": \"Hello World\"}");\n'\
+'\t}\n'\
+'}'\
+>> ./src/main/java/init/Server.java
+
+echo -e \
+"apply plugin: 'application'\n"\
+"mainClassName = 'init/Server'\n"\
+"repositories {\n"\
+"\tmavenCentral()\n"\
+"}\n"\
+"dependencies {\n"\
+"\t// https://mvnrepository.com/artifact/com.sparkjava/spark-core\n"\
+"\tcompile group: 'com.sparkjava', name: 'spark-core', version: '2.7.2'\n"\
+"}"\
+>> build.gradle
+
+# 起動
+gradle run
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 各言語で最小構成のserverを構築する
 
-1. [java](java_gretty.md)
-2. [java](java_spark.md)
-3. [nodejs](nodejs_express.md)
-4. [python](python_flask.md)
+1. [java](text_java_gretty.md)
+2. [java](text_java_spark.md)
+3. [nodejs](text_nodejs_express.md)
+4. [python](text_python_flask.md)
