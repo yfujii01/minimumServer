c37d94ff8f5bbc1c4f4a57c26daaf9a6f7da1a57
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-06-07 14:34:36 +0000

    https対応のruby thinを追加

--- readme.md
+++ readme.md
@@ -11,3 +11,5 @@ shellを実行してください
 
 curl -L raw.github.com/yfujii01/minimumServer/master/ruby_sinatra_unicorn/make.sh | sh -s -- sample_rubyapp 3000
 
+curl -L raw.github.com/yfujii01/minimumServer/master/ruby_sinatra_thin/make.sh | sh -s -- sample_rubyapp 3000
+
--- ruby_sinatra_thin/make.sh
+++ ruby_sinatra_thin/make.sh
@@ -13,14 +13,11 @@ rm -rf ${appname}
 mkdir ${appname}
 cd ${appname}
 
-#gem install
-#gem install bundler
-
 bundle init
 
 cat << EOS >> Gemfile
 gem "sinatra"
-gem "unicorn"
+gem 'thin'
 gem "json"
 EOS
 
@@ -28,56 +25,52 @@ bundle install
 
 # 実行ファイル作成
 cat << EOS > app.rb
-require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 
-get '/' do
-  "Hello World!!"
-end
+class Application < Sinatra::Base
+  configure do
+    set :bind, '0.0.0.0'
+    set :port, ${appport}
+  end
 
-get '/abc' do
-  "ABC Hello World!!"
-end
+  get '/' do
+    'Hello, SSL.'
+  end
 
-# 実行例
-# curl -H 'Content-Type:application/json' -d '{"asd":"wet"}' localhost:${appport}/posttest
-post '/posttest' do
-  params = JSON.parse request.body.read
-  puts params
-  puts 'hello'
-  "BC Hello World!!" + params.to_s
+  # 実行例
+  # curl -k -H 'Content-Type:application/json' -d '{"asd":"wet"}' https://localhost:${appport}/posttest
+  post '/posttest' do
+    params = JSON.parse request.body.read
+    puts params
+    puts 'hello'
+    "Hello World!!" + params.to_s
+  end
+
+  def self.run!
+    super do |server|
+      server.ssl = true
+      server.ssl_options = {
+        :cert_chain_file  => File.dirname(__FILE__) + "/server.crt",
+        :private_key_file => File.dirname(__FILE__) + "/server.key",
+        :verify_peer      => false
+      }
+    end
+  end
+
+  run! if app_file == \$0
 end
 EOS
 
-cat << EOS > config.ru
-require './app.rb'
-run Sinatra::Application
-EOS
+#オレオレ証明書作成
+curl -L raw.github.com/yfujii01/setting_oreoreSSL/master/make.sh | sh
 
-cat << EOS > unicorn.conf
-# ワーカーの数
-worker_processes 2
-
-# ソケット
-listen '`pwd`/tmp/unicorn-lokka.sock'
-listen ${appport}, :tcp_nopush => false
-
-# ログ(ログに出力する場合は以下のコメントを外す)
-# stderr_path 'log/unicorn.log'
-# stdout_path 'log/unicorn.log'
-EOS
-
-mkdir log
-mkdir tmp
 
 cat << EOS > start.sh
 #!/bin/bash
-
 export PATH="$PATH"
-
 cd \`dirname \$0\`
-bundle exec unicorn -c unicorn.conf
+bundle exec ruby app.rb
 EOS
 
 chmod +x start.sh
@@ -85,13 +78,11 @@ chmod +x start.sh
 cat << EOS > ${appname}.service
 [Unit]
 Description = ${appname}
-
 [Service]
 ExecStart=`pwd`/start.sh
 Restart=always
 Type=simple
 User=`whoami`
-
 [Install]
 WantedBy=multi-user.target
 EOS
@@ -101,32 +92,27 @@ cat << EOS > serviceMemo.txt
 --------------------
 #サーバーを起動する
 ./${appname}/start.sh
-
 #サービスを削除する
 sudo systemctl stop ${appname}
 sudo systemctl daemon-reload
 sudo rm /etc/systemd/system/${appname}.service
-
 #サービスを登録する
 sudo cp ./${appname}/${appname}.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start ${appname}
-
 #サービスの状態を確認する
 systemctl status ${appname}
 journalctl -xe
-
 #サービスを自動起動する
 sudo systemctl is-enabled ${appname}
 sudo systemctl enable ${appname}
 sudo systemctl disable ${appname}
 --------------------
 #動作確認する
-curl -H 'Content-Type:application/json' -d '{"asd":"wet"}' localhost:${appport}/posttest
+curl -k -H 'Content-Type:application/json' -d '{"asd":"wet"}' https://localhost:${appport}/posttest
 EOS
 
 cat serviceMemo.txt
 
 # 初期ディレクトリに戻る
 cd ..
-
